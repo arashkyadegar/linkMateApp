697736ebbf6ca1c4228ca8207003d577e3f076cd
arashk <yadegar.arashk@gmail.com>
2025-04-01 05:09:03 -0700

    feat:AutoExpreLink context added

---
+++ src/components/context/darkmodcontext.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+// Create the context
+const DarkModeContext = createContext(undefined);
+
+// Define the provider component
+export const DarkModeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedMode = localStorage.getItem("darkMode");
+    return savedMode === "true"; // Convert to boolean
+  });
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("darkMode", darkMode.toString());
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+// Hook to use the dark mode context
+export const useDarkMode = () => {
+  const context = useContext(DarkModeContext);
+  if (context === undefined) {
+    throw new Error("useDarkMode must be used within a DarkModeProvider");
+  }
+  return context;
+};
